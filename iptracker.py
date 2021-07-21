ebb9cce071fe4f998e618fee87337d7235b913d7
Pedro <pmapm@ua.pt>
2021-07-21 11:36:03 +0100

    show coords in map

--- iptracker.py
+++ iptracker.py
@@ -1,12 +1,11 @@
-#890 () [] } =
-
-import sys, requests, json
+import sys, requests, json, folium, webbrowser
 
 ip = input('Ip target: ')
 
 try:
     response = requests.get(f"http://ip-api.com/json/{ip}").content
     data = json.loads(response)
+    
     print("Ip: "+data['query'])
     print("City: "+data['city'])
     print("Country: "+data['country'])
@@ -14,6 +13,12 @@ try:
     print("Lon: {}".format(float(data['lon'])))
     print("ISP: "+data['isp'])
     print("AS: "+data['as'])
+    
+    filepath = '/home/pedro/Desktop/Programar/iptacker/map.html'
+    m = folium.Map(location=[float(data['lat']), float(data['lon'])], zoom_start=10.5)
+    folium.Marker([float(data['lat']), float(data['lon'])], popup=ip).add_to(m)
+    m.save(filepath)
+    webbrowser.open('file://' + filepath)
      
 except KeyboardInterrupt:
     sys.exit(0)    
